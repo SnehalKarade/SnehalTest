a299494c999908fab78eb9a95412c0e200fdd11f
BHAGYASHRI <BHAGYASHRI@192.168.0.17>
2019-01-31 14:41:00 +0530

    by Snehal

--- Selenium/src/test/java/myJenkins/Selenium/TestClass.java
+++ Selenium/src/test/java/myJenkins/Selenium/TestClass.java
@@ -1,8 +1,13 @@
 package myJenkins.Selenium;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -15,22 +20,39 @@ public void login()
 {
 System.setProperty("webdriver.chrome.driver","D:\\WorkSpace_1\\chromedriver.exe");
 driver = new ChromeDriver();
-driver.get("https://demo.vtiger.com");
+driver.get("https://labs.abeautifulsite.net/archived/jquery-multiSelect/demo/");
+driver.manage().window().maximize();
+driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 }
 @Test
 public void enterData()
 	{
-driver.findElement(By.id("username")).sendKeys("admin");
-
-driver.findElement(By.id("password")).sendKeys("admin");
+WebElement mutiselect = driver.findElement(By.id("control_6"));
+	
+	Select objMultiselect = new Select(mutiselect);
+	
+	objMultiselect.selectByIndex(1);
+	objMultiselect.selectByValue("option_3");
+	objMultiselect.selectByVisibleText("Option 2");
+	
+	List<WebElement> selectedOptions = objMultiselect.getAllSelectedOptions();
+	
+	int size = selectedOptions.size();
+	System.out.println(size);
+	
+	for(int i = 0;i<size;i++) {
+		
+		System.out.println("Selected option "+i+" : " +selectedOptions.get(i).getText());
+	}
+	
+	objMultiselect.deselectAll();
 }
 
 @AfterTest
 public void tearDown()
 {
 	driver.close();
-	System.out.println("Hello");
-	System.out.println("hi");
+	
 }
 
 }
