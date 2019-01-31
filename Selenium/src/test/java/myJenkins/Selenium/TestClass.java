1b01f51d3cd2c8c0226de01685ad7b739cc0c67a
BHAGYASHRI <BHAGYASHRI@192.168.0.17>
2019-01-31 16:01:39 +0530

    by miss neha

--- Selenium/src/test/java/myJenkins/Selenium/TestClass.java
+++ Selenium/src/test/java/myJenkins/Selenium/TestClass.java
@@ -47,6 +47,7 @@ WebElement mutiselect = driver.findElement(By.id("control_6"));
 	
 	objMultiselect.deselectAll();
    System.out.println("SK");
+   System.out.println("Testing done");
 }
 
 @AfterTest
